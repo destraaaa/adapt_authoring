6818bd0a58329ef6dd520cb75313136910e405b9
aryl000 <aryl.otniel@tokopedia.com>
2018-07-24 16:10:22 +0700

    fix publish and post data

--- routes/download/index.js
+++ routes/download/index.js
@@ -11,10 +11,14 @@ var path = require('path');
 var OutputPlugin = require('../../lib/outputmanager').OutputPlugin;
 var util = require('util');
 var unzip = require('unzip');
+var async = require('async');
 var fstream = require('fstream')
+var MongoClient = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
+var url = "mongodb://localhost:27017/";
+var axios = require('axios');
 
-function DownloadOutput() {
-}
+// function DownloadOutput() {}
 
 util.inherits(DownloadOutput, OutputPlugin);
 
@@ -101,11 +105,12 @@ server.get('/download/:tenant/:course/:title/download.zip', function (req, res, 
 server.get('/download/:tenant/:course/:title/download', function (req, res, next) {
   var tenantId = req.params.tenant;
   var courseId = req.params.course;
-  var courseName = '/'+req.params.title;
+  var courseName = '/' + req.params.title;
+  var createdBy = req.user.email;
   var FRAMEWORK_ROOT_FOLDER = path.join(configuration.tempDir, configuration.getConfig('masterTenantID'), Constants.Folders.Framework);
   var downloadZipFilename = path.join(FRAMEWORK_ROOT_FOLDER, Constants.Folders.AllCourses, tenantId, courseId, Constants.Filenames.Download);
   var downloadBuildFilename = path.join(FRAMEWORK_ROOT_FOLDER, Constants.Folders.AllCourses, tenantId, courseId, Constants.Filenames.Build);
-  var downloadNewFilename = path.join(FRAMEWORK_ROOT_FOLDER, Constants.Folders.AllCourses, tenantId, courseId,courseName);
+  var downloadNewFilename = path.join(FRAMEWORK_ROOT_FOLDER, Constants.Folders.AllCourses, tenantId, courseId, courseName);
   var currentUser = usermanager.getCurrentUser();
 
 
@@ -117,7 +122,66 @@ server.get('/download/:tenant/:course/:title/download', function (req, res, next
 
         next(err);
       } else {
+        MongoClient.connect(url, function (err, db) {
+          if (err) throw err;
+          var dbo = db.db("adapt-tenant-master");
+          dbo.collection("courses").findOne({
+            _id: new objectId(req.params.course)
+          }, (function (err, result) {
+            if (err) throw err;
+            var tagLength = result.tags.length;
+            tagInit = 0;
+            tagging = [];
+            heroImageId = "";
+           
+            dbo.collection("assets").findOne({
+                _id: new objectId(result.heroImage)
+              }, (function (err, result) {
+                if (err) throw err;
+                if (result == null)
+                  heroImageId = "";
+                else heroImageId = courseName+result.path;
+              })),
+              result.tags.forEach(function (item) {
+                dbo.collection("tags").findOne({
+                  _id: new objectId(item)
+                }, (function (err, result) {
+                  if (err) throw err;
+                  tagging.push(result.title);
+                  tagInit++;
+                  if (tagInit == tagLength) { 
+                    var authOptions = {
+                      method: 'POST',
+                      url: '/api/course/add',
+                      proxy: {
+                        host: '192.168.245.89',
+                        port: 8080
+                    },
+                      data: {
+                        id: req.params.course,
+                        title: req.params.title,
+                        categories: tagging.toString(),
+                        createdBy: createdBy,
+                        image: heroImageId
+                      },
+                      headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                      },
+                      json: true
+                    };
+                    axios(authOptions)
+                      .catch(function (error) {
+                        console.log(error);
+                      });
+                  }
+                }));
+              });
+            db.close();
+          }));
+        });
+
         fse.copySync(downloadBuildFilename, downloadNewFilename);
+        res.redirect('back');
       }
     });
   } else {
